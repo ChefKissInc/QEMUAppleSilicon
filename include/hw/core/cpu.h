acb94d16d9f3b75dae8de50da95f52cbc19b1872
Richard Henderson <richard.henderson@linaro.org>
2025-07-11 16:56:22 -0600

    include/hw/core/cpu: Introduce MMUIdxMap
    Use a typedef instead of uint32_t directly when
    describing sets of mmu indexes.

--- include/hw/core/cpu.h
+++ include/hw/core/cpu.h
@@ -198,10 +198,10 @@ struct CPUClass {
 };
 
 /*
- * Fix the number of mmu modes to 32, which is also the maximum
- * supported by the softmmu tlb api.
+ * Fix the number of mmu modes to 16.
  */
 #define NB_MMU_MODES 32
+typedef uint32_t MMUIdxMap;
 
 /* Use a fully associative victim tlb of 8 entries. */
 #define CPU_VTLB_SIZE 8
@@ -307,7 +307,7 @@ typedef struct CPUTLBCommon {
      * mmu_idx N since the last time that mmu_idx was flushed.
      * Protected by tlb_c.lock.
      */
-    uint16_t dirty;
+    MMUIdxMap dirty;
     /*
      * Statistics.  These are not lock protected, but are read and
      * written atomically.  This allows the monitor to print a snapshot
