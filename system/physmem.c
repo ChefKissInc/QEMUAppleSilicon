9f077dc6b66428b4816a8ff1a653a809c6e9d31e
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2026-03-03 19:33:33 +0200

    fix(system/physmem): rogue line added by linus Gittard

--- system/physmem.c
+++ system/physmem.c
@@ -794,7 +794,6 @@ void cpu_destroy_address_spaces(CPUState *cpu)
     int asidx;
 
     assert(cpu->cpu_ases);
-    assert(asidx >= 0 && asidx < cpu->num_ases);
 
     /* convenience alias just points to some cpu_ases[n] */
     cpu->as = NULL;
