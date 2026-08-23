ChefKissInc/QEMUAppleSilicon

commit b4d4f0a8d35ef1f4396ee3fb07292f9cbafca9fc

hw/core/cpu: make mmu nodes only 16 again
gxf bit takes M-profile's place, now that it is removed

include/hw/core/cpu.h
@@ -191,10 +191,10 @@ struct CPUClass
 };
 
 /*
- * Fix the number of mmu modes to 16.
+ * Fix the number of mmu modes.
  */
-#define NB_MMU_MODES 32
-typedef uint32_t MMUIdxMap;
+#define NB_MMU_MODES 16
+typedef uint16_t MMUIdxMap;
 
 /* Use a fully associative victim tlb of 8 entries. */
 #define CPU_VTLB_SIZE 8

target/arm/cpu.h
@@ -2564,15 +2564,13 @@ bool write_cpustate_to_list(ARMCPU* cpu, bool kvm_sync);
  * The constant names here are patterned after the general style of the names
  * of the AT/ATS operations.
  * The values used are carefully arranged to make mmu_idx => EL lookup easy.
- * For M profile we arrange them to have a bit for priv, a bit for negpri
- * and a bit for secure.
  */
-#define ARM_MMU_IDX_A_GXF 0x20
-#define ARM_MMU_IDX_A     0x40 /* A profile */
-#define ARM_MMU_IDX_NOTLB 0x80 /* does not have a TLB */
+#define ARM_MMU_IDX_A     0x10 /* A profile */
+#define ARM_MMU_IDX_NOTLB 0x20 /* does not have a TLB */
+#define ARM_MMU_IDX_A_GXF 0x40
 
-#define ARM_MMU_IDX_TYPE_MASK    (ARM_MMU_IDX_A | ARM_MMU_IDX_NOTLB)
-#define ARM_MMU_IDX_COREIDX_MASK 0x1f
+#define ARM_MMU_IDX_TYPE_MASK    (ARM_MMU_IDX_A_GXF | ARM_MMU_IDX_A | ARM_MMU_IDX_NOTLB)
+#define ARM_MMU_IDX_COREIDX_MASK 0xf
 
 typedef enum ARMMMUIdx
 {

commit 0aee02daa41ec995d980ba3d2089c505045c6b5a

whoops, I just broke gxf in `b4d4f0a`
revert the mmuidxmap to u32, but set mmu_modes to 26 instead of 32, as the max value that arises in armmuidx is 25 (ARMMMUIdx_GE30_3_PAN). also add a compile assert to ensure this never happens again
chris as co-author because he told me to look into some tcg fix I did in 2025
Co-authored-by: Christian Inci <chris.gh@broke-the-inter.net>

include/hw/core/cpu.h
@@ -193,8 +193,8 @@ struct CPUClass
 /*
  * Fix the number of mmu modes.
  */
-#define NB_MMU_MODES 16
-typedef uint16_t MMUIdxMap;
+#define NB_MMU_MODES 26
+typedef uint32_t MMUIdxMap;
 
 /* Use a fully associative victim tlb of 8 entries. */
 #define CPU_VTLB_SIZE 8

target/arm/cpu.h
@@ -2565,12 +2565,12 @@ bool write_cpustate_to_list(ARMCPU* cpu, bool kvm_sync);
  * of the AT/ATS operations.
  * The values used are carefully arranged to make mmu_idx => EL lookup easy.
  */
-#define ARM_MMU_IDX_A     0x10 /* A profile */
-#define ARM_MMU_IDX_NOTLB 0x20 /* does not have a TLB */
-#define ARM_MMU_IDX_A_GXF 0x40
+#define ARM_MMU_IDX_A_GXF 0x10
+#define ARM_MMU_IDX_A     0x40 /* A profile */
+#define ARM_MMU_IDX_NOTLB 0x80 /* does not have a TLB */
 
-#define ARM_MMU_IDX_TYPE_MASK    (ARM_MMU_IDX_A_GXF | ARM_MMU_IDX_A | ARM_MMU_IDX_NOTLB)
-#define ARM_MMU_IDX_COREIDX_MASK 0xf
+#define ARM_MMU_IDX_TYPE_MASK    (ARM_MMU_IDX_A | ARM_MMU_IDX_NOTLB)
+#define ARM_MMU_IDX_COREIDX_MASK 0x1f
 
 typedef enum ARMMMUIdx
 {
@@ -2621,6 +2621,8 @@ typedef enum ARMMMUIdx
     ARMMMUIdx_Stage1_GE1     = 3 | ARM_MMU_IDX_NOTLB,
     ARMMMUIdx_Stage1_GE1_PAN = 4 | ARM_MMU_IDX_NOTLB,
 } ARMMMUIdx;
+
+QEMU_BUILD_BUG_ON((ARMMMUIdx_Phys_Realm & ARM_MMU_IDX_COREIDX_MASK) >= ARM_MMU_IDX_A_GXF);
 
 /*
  * Bit macros for the core-mmu-index values for each index,