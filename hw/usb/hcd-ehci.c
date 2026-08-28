004eeb209a7d7974f1fbaba1e91dcb9f9e50cf21
Visual <30368284+VisualEhrmanntraut@users.noreply.github.com>
2026-08-28 19:26:43 +0300

    ehci: formatting

--- hw/usb/hcd-ehci.c
+++ hw/usb/hcd-ehci.c
@@ -1515,7 +1515,9 @@ static int ehci_state_fetchsitd(EHCIState* ehci, int async)
     if (get_dwords(ehci, NLPTR_GET(entry), (uint32_t*)&sitd, sizeof(EHCIsitd) >> 2) < 0) { return 0; }
     ehci_trace_sitd(ehci, entry, &sitd);
 
-    if (!(sitd.results & SITD_RESULTS_ACTIVE)) { /* siTD is not active, nothing to do */; }
+    if (!(sitd.results & SITD_RESULTS_ACTIVE)) {
+        /* siTD is not active, nothing to do */
+    }
     else {
         /* TODO: split transfers are not implemented */
         warn_report("Skipping active siTD");
