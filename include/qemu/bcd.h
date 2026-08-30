1b34e3da2a1d088b049aa09d217b8e1f952ce437
Visual <30368284+VisualEhrmanntraut@users.noreply.github.com>
2026-08-30 13:21:42 +0300

    remove more leftover files

--- include/qemu/bcd.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-/* Convert a byte between binary and BCD.  */
-static inline uint8_t to_bcd(uint8_t val) { return ((val / 10) << 4) | (val % 10); }
-
-static inline uint8_t from_bcd(uint8_t val) { return ((val >> 4) * 10) + (val & 0x0f); }
--- ui/qemu-x509.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define X509_CA_CERT_FILE     "ca-cert.pem"
-#define X509_CA_CRL_FILE      "ca-crl.pem"
-#define X509_SERVER_KEY_FILE  "server-key.pem"
-#define X509_SERVER_CERT_FILE "server-cert.pem"
